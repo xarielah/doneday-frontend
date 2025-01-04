48c4b333c675c739f8fb10d1135376d69f6d40db
afikyefet <afik.yefet@gmail.com>
2025-01-04 23:57:37 +0200

    appheader clean

--- src/cmps/AppHeader.jsx
+++ src/cmps/AppHeader.jsx
@@ -8,6 +8,7 @@ export function AppHeader() {
 	const user = useSelector(storeState => storeState.userModule.user)
 	const navigate = useNavigate()
 
+	
 	async function onLogout() {
 		try {
 			await logout()
@@ -21,27 +22,6 @@ export function AppHeader() {
 	return (
 		<header className="app-header full">
 			<nav>
-				<NavLink to="/" className="logo">
-					E2E Demo
-				</NavLink>
-				<NavLink to="about">About</NavLink>
-				<NavLink to="car">Cars</NavLink>
-				<NavLink to="chat">Chat</NavLink>
-				<NavLink to="review">Review</NavLink>
-
-                {user?.isAdmin && <NavLink to="/admin">Admin</NavLink>}
-
-				{!user && <NavLink to="login" className="login-link">Login</NavLink>}
-				{user && (
-					<div className="user-info">
-						<Link to={`user/${user._id}`}>
-							{/* {user.imgUrl && <img src={user.imgUrl} />} */}
-							{user.fullname}
-						</Link>
-						{/* <span className="score">{user.score?.toLocaleString()}</span> */}
-						<button onClick={onLogout}>logout</button>
-					</div>
-				)}
 			</nav>
 		</header>
 	)
