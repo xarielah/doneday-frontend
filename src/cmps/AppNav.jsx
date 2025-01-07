bd96e73499fee2dbd82a395633d8244329435c1e
afikyefet <afik.yefet@gmail.com>
2025-01-07 23:14:35 +0200

    app nav bar design

--- src/cmps/AppNav.jsx
+++ src/cmps/AppNav.jsx
@@ -1,5 +1,78 @@
+import { Link, NavLink } from "react-router-dom";
+
+
 export function AppNav(){
     return (
-        <section className="app-nav">test nav</section>
+        <section className="app-nav">
+            <nav className="nav">
+
+                <section className="nav-section">
+                    <NavLink to={"/1"}>
+                        <div className="icon">
+                            <img src="/public/img/monday-icons/asset 21.svg" alt="" />
+                        </div>
+                        <span>Home</span>
+                    </NavLink>
+
+                    <NavLink to={"/2"}>
+                        <div className="icon">
+                            <img src="/public/img/monday-icons/asset 22.svg" alt="" />
+                        </div>
+                        <span>My work</span>
+                    </NavLink>
+                </section>
+
+                <div className="divider"></div>
+
+                <section className="nav-section favorite">
+                    <Link to={"/3"}>
+                        <div className="icon-star">
+                           <img src="/public/img/monday-icons/asset 34.svg" alt="" />
+                      </div>
+                      <span>Favorites</span>
+                        <div className="icon">
+                           <img src="/public/img/monday-icons/asset 28.svg" alt="" />
+                      </div>
+                      
+                    </Link>
+                </section>
+
+                <div className="divider"></div>
+
+                <section className="nav-section">
+                    <Link to={"/4"}>
+                        <div className="icon">
+                            <img src="/public/img/monday-icons/asset 24.svg" alt="" />
+                        </div>
+                        <span>Workspaces</span>
+                    </Link>
+
+                    <Link to={"/4"}>
+                        <div className="icon">
+                            <img src="/public/img/monday-icons/asset 24.svg" alt="" />
+                        </div>
+                        <span>Main Workspaces</span>
+                    </Link>
+                </section>
+
+                <section className="nav-section">
+                    <NavLink to={"/5"}>
+                        <div className="icon">
+                            <img src="/public/img/monday-icons/asset 36.svg" alt="" />
+                        </div>
+                        <span>monday recreate</span>
+                    </NavLink>
+
+                    <NavLink to={"/6"}>
+                        <div className="icon">
+                            <img src="/public/img/monday-icons/asset 31.svg" alt="" />
+                        </div>
+                        <span>Dashboard and reporting</span>
+                    </NavLink>
+                </section>
+
+
+            </nav>
+        </section>
     )
 }
