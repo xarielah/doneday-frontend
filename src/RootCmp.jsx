abe8c8753ae63a12871d530603b4ce184e9c0ec9
Ariel Aharon <xarielah@gmail.com>
2025-01-15 22:17:54 +0200

    feat: updating root cmp with authed or unauthed routes

--- src/RootCmp.jsx
+++ src/RootCmp.jsx
@@ -1,26 +1,40 @@
 import "@vibe/core/tokens"
-import { Route, Routes } from 'react-router'
+import { Navigate, Route, Routes } from 'react-router'
 import { AppFooter } from './cmps/AppFooter'
 import { AppHeader } from './cmps/AppHeader'
-import { AppNav } from './cmps/AppNav.jsx'
-import { Board } from './pages/Board.jsx'
-import { HomePage } from './pages/HomePage.jsx'
+import { AppNav } from './cmps/AppNav'
+import { Board } from './pages/Board'
+import ExternalHomePage from "./pages/ExternalHomePage"
+import { HomePage } from './pages/HomePage'
 
 export function RootCmp() {
-    return (
-        <div className="main-container">
-            <AppHeader />
-            <AppNav />
-            <main>
-                <Routes>
-                    <Route path="" element={<HomePage />} />
-                    <Route path='board' element={<Board />} />
-                    <Route path='board/:boardId' />
-                </Routes>
-            </main>
-            <AppFooter />
-        </div>
-    )
+    const isAuthenticated = false;
+    if (isAuthenticated) return <AuthenticatedRoutes />
+    else return <UnauthenticatedRoutes />
+}
+
+const AuthenticatedRoutes = () => {
+    return <div className="main-container">
+        <AppHeader />
+        <AppNav />
+        <main>
+            <Routes>
+                <Route path="" element={<HomePage />} />
+                <Route path='board' element={<Board />} />
+                <Route path='board/:boardId' />
+            </Routes>
+        </main>
+        <AppFooter />
+    </div>
+}
+
+const UnauthenticatedRoutes = () => {
+    return <div className="external-main-container">
+        <Routes>
+            <Route path="" element={<ExternalHomePage />} />
+            <Route path='*' element={<Navigate to="/" replace />} />
+        </Routes>
+    </div>
 }
 
 
