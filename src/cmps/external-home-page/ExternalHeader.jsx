53b821aa658acb81881c474c7f0ad117e523affc
Ariel Aharon <xarielah@gmail.com>
2025-01-15 22:16:37 +0200

    feat: adding header to external users home page

---
+++ src/cmps/external-home-page/ExternalHeader.jsx
@@ -0,0 +1,57 @@
+import { useEffect, useRef } from "react";
+import { useNavigate } from "react-router";
+import { Link } from "react-router-dom";
+import ExternalDemoButton from "./ExternalDemoButton";
+
+const ExternalHeader = () => {
+    const navigate = useNavigate();
+    const headerRef = useRef()
+    const baseDivRef = useRef()
+
+    useEffect(() => {
+        const observer = new IntersectionObserver((entries) => {
+            entries.forEach((entry) => {
+                if (entry.isIntersecting) {
+                    headerRef.current.classList.remove("external-header-sticky");
+                } else {
+                    headerRef.current.classList.add("external-header-sticky");
+                }
+            });
+        });
+        observer.observe(baseDivRef.current);
+    }, [])
+
+    return (
+        <>
+            <div id="intersection-element" style={{ position: 'absolute', top: '0' }} ref={baseDivRef}></div>
+            <header className="external-header" ref={headerRef}>
+                <div className="external-header-container">
+                    <Link to="/">
+                        <img src="/public/img/logo-transparent.png" alt="logo" className="external-header-logo" />
+                    </Link>
+                    <nav className="external-nav">
+                        <section className="external-nav-first-menu">
+                            <a
+                                className="external-header-button"
+                                href="https://github.com/afikyefet/monday-clone-proj"
+                                target="_blank">
+                                Source code
+                            </a>
+                        </section>
+                        <section className="external-nav-other-options">
+                            <span
+                                role='link'
+                                className="external-header-button"
+                                onClick={() => navigate("/login")}>
+                                Log in
+                            </span>
+                            <ExternalDemoButton />
+                        </section>
+                    </nav>
+                </div>
+            </header >
+        </>
+    )
+}
+
+export default ExternalHeader;
