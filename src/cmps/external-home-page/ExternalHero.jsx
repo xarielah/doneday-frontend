cd2028fab0b823dbd1f1425d08a6788b3046c253
Ariel Aharon <xarielah@gmail.com>
2025-01-15 22:16:20 +0200

    feat: adding hero to the main page

---
+++ src/cmps/external-home-page/ExternalHero.jsx
@@ -0,0 +1,31 @@
+import { Heading, Text } from "@vibe/core";
+import ExternalDemoButton from "./ExternalDemoButton";
+
+const ExternalHero = () => {
+    return (
+        <main className="external-hero">
+            <div className="external-hero-segment">
+                <article className="external-hero-segment-content">
+                    <Heading
+                        align="center"
+                        weight="light"
+                        type="h1"
+                        className="external-hero-segment-title">
+                        Made for work,<br />
+                        designed to love
+                    </Heading>
+                    <Text
+                        element="p"
+                        color="secondary"
+                        align="center"
+                        style={{ fontWeight: 300, lineHeight: '160%', fontSize: '1.125rem', maxWidth: '540px', height: '96px' }}>
+                        Streamline workflows and gain clear visibility across teams to make strategic decisions with confidence.
+                    </Text>
+                    <ExternalDemoButton size="large" />
+                </article>
+            </div>
+        </main>
+    )
+}
+
+export default ExternalHero;
