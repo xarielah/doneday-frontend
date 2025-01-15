7b6c3073c5adbee81b2a025ccf8479fa0e5e9e6c
Ariel Aharon <xarielah@gmail.com>
2025-01-15 22:16:49 +0200

    feat: adding footer to external users home page

---
+++ src/cmps/external-home-page/ExternalFooter.jsx
@@ -0,0 +1,12 @@
+const ExternalFooter = () => {
+    return (
+        <footer className="external-footer">
+            <div className="external-footer-left-section">
+                <img src="/public/img/logo.png" alt="logo" className="external-footer-logo" />
+            </div>
+            <div className="external-footer-right-section"></div>
+        </footer>
+    )
+}
+
+export default ExternalFooter;
