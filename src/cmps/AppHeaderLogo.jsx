5ae2499fe1bb7919ea9a9dd6287f51803ccd43dd
Ariel Aharon <xarielah@gmail.com>
2025-01-19 22:56:34 +0200

    updating logo in board to new logo

--- src/cmps/AppHeaderLogo.jsx
+++ src/cmps/AppHeaderLogo.jsx
@@ -1,14 +1,10 @@
-import { Text } from "@vibe/core";
 import { Link } from "react-router-dom";
 
 const AppHeaderLogo = () => {
     return <section className='app-header-logo'>
         <Link to='/'>
             <span className='app-header-text-and-img'>
-                <img src="https://cdn.monday.com/images/logos/monday_logo_icon.png" alt="Doneday logo" className='company-logo' style={{ display: 'inline' }} />
-                <Text type='text1' element='h1' color='primary' style={{ margin: '0' }}>
-                    <b>monday</b><span style={{ fontWeight: '300' }}>.com</span>
-                </Text>
+                <img src="/img/logo-transparent.png" alt="Doneday logo" className='company-logo-header' style={{ display: 'inline', minWidth: 'max-content' }} />
             </span>
         </Link>
     </section>
