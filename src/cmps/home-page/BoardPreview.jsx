8e960a6ba196f01c0f0f57850aaacfc9f3191ab9
= <dorcohen236@gmail.com>
2025-01-21 20:06:22 +0200

    responsive fixed plus navlink

--- src/cmps/home-page/BoardPreview.jsx
+++ src/cmps/home-page/BoardPreview.jsx
@@ -1,23 +1,26 @@
 import { Heading, Box, Avatar, Icon, Text } from "@vibe/core";
 import { Info, Favorite } from "@vibe/icons";
+import { NavLink } from 'react-router-dom'
 
 export function BoardPreview({board}) {
     return (
-        <Box className="box" rounded="medium" key={board.id}>
-            <div className="box-icon">
-                <Avatar withoutBorder size="large" type="icon" icon={Info} backgroundColor={board.squareColor} square />
-            </div>
-            <div className="box-content">
-                <Heading type="h4" color="primary" className="box-header">
-                    {board.title}
-                </Heading>
-                <Text>
-                    <div className="box-desc">
-                        <Icon icon={Favorite} iconSize={20} />
-                        <span>{board.subTitle}</span>
-                    </div>
-                </Text>
-            </div>
-        </Box>
+        <NavLink to={`/board/${board.id}`} className="box-link">
+            <Box className="box" rounded="medium" key={board.id}>
+                <div className="box-icon">
+                    <Avatar withoutBorder size="large" type="icon" icon={Info} backgroundColor={board.squareColor} square />
+                </div>
+                <div className="box-content">
+                    <Heading type="h4" color="primary" className="box-header">
+                        {board.title}
+                    </Heading>
+                    <Text>
+                        <div className="box-desc">
+                            <Icon icon={Favorite} iconSize={20} />
+                            <span>{board.subTitle}</span>
+                        </div>
+                    </Text>
+                </div>
+            </Box>
+        </NavLink>
     )
 }
