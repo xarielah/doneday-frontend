e2073284f8878515a486f2d2a348ed8646f198da
afikyefet <afik.yefet@gmail.com>
2025-01-22 19:27:40 +0200

    select start

--- src/cmps/board cmps/structure/GroupTableContentTask.jsx
+++ src/cmps/board cmps/structure/GroupTableContentTask.jsx
@@ -1,20 +1,40 @@
 import { EditableText } from "@vibe/core"
-import { updateTask } from "../../../store/actions/board.actions"
+import { addSelectedTask, removeSelectedTask, updateTask } from "../../../store/actions/board.actions"
 import DynamicColumn from "./DynamicColumn"
 import GroupPreRow from "./GroupPreRow"
 import GroupScrollableColumns from "./GroupScrollableColumns"
 import GroupStickyColumns from "./GroupStickyColumns"
+import { useSelector } from "react-redux"
 
 const GroupTableContentTask = ({ task, columnLabels, group }) => {
+    const selectedTasks = useSelector(storeState => storeState.boardModule.selectedTasks)
 
     const handleCellUpdate = (cmpType, value) => {
         const updatedTask = { ...task, [cmpType]: value }
         updateTask(group._id, updatedTask)
     }
 
+    async function handleChangeSelect(ev, groupId, taskId) {
+        if (ev.target.checked) {
+            await addSelectedTask(groupId, taskId)
+        } else {
+            await removeSelectedTask(groupId, taskId)
+        }
+    }
+
+    function isTaskSelected(groupId = "", taskId = "") {
+        const group = selectedTasks.find(selectedGroups => selectedGroups.groupId === groupId)
+        if (!group) return false
+        return group.tasks.includes(taskId)
+    }
+
     return (<div role="listitem" className="table-task-row">
         <GroupStickyColumns>
-            <GroupPreRow group={group} />
+            <GroupPreRow
+                isChecked={isTaskSelected(group._id, task._id)}
+                onCheckBox={(ev) => handleChangeSelect(ev, group._id, task._id)}
+                group={group}
+            />
             <div className="min-table-cell table-cell-first-column">
                 <EditableText type="text2" value={task.taskTitle} />
             </div>
--- src/cmps/board cmps/structure/GroupTableFooter.jsx
+++ src/cmps/board cmps/structure/GroupTableFooter.jsx
@@ -1,17 +1,30 @@
 import { EditableText } from "@vibe/core"
 import GroupPreRow from "./GroupPreRow"
 import GroupStickyColumns from "./GroupStickyColumns"
+import { addTask } from "../../../store/actions/board.actions"
+import { boardService } from "../../../services/board/board.service.local"
 
 const GroupTableFooter = ({ onAddTask, group }) => {
+
+    function onAddTask(groupId, taskTitle) {
+        const newTask = boardService.getEmptyTask()
+        newTask.taskTitle = taskTitle
+        return addTask(groupId, newTask)
+    }
+
+    function onBlurTitle(taskTitle) {
+        onAddTask(group._id, taskTitle)
+    }
+
     return <section className="table-footer">
         <GroupStickyColumns>
             <GroupPreRow group={group} roundedBottomLeft bottomBorders disableCheckbox />
             <div className="min-table-cell add-task-cell cell-left-padding task-border-bottom" style={{ textAlign: 'left' }}>
                 <EditableText
                     onClick={onAddTask}
-                    onChange={taskTitle => onAddTask(taskTitle)}
+                    onChange={taskTitle => onBlurTitle(taskTitle)}
                     className="cell-left-padding"
-                    placeholder="Add Task"
+                    placeholder="+ Add Task"
                 />
             </div>
         </GroupStickyColumns>
