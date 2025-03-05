47e5ee96ebe3b266c64c596d5a1920dcc09f366d
Afik Yefet <afik.yefet@gmail.com>
2025-03-05 16:52:29 +0200

    sort progress

--- src/cmps/board cmps/BoardSort/SortBody.jsx
+++ src/cmps/board cmps/BoardSort/SortBody.jsx
@@ -1,16 +1,12 @@
 import React from "react";
-import { Text } from "@vibe/core";
+import { Dropdown, Text } from "@vibe/core";
 import SortRow from "./SortRow";
 
 const SortBody = () => {
     return (
-        <section className="filter-body">
-            <section className="filter-body-content">
-                {/* {columns.map((column, idx) => (
-                    <SortRow />
-                ))} */}
-            </section>
-        </section>
+        <section className="sort-body">
+            <SortRow />
+        </section >
     );
 };
 
--- src/cmps/board cmps/BoardSort/SortRow.jsx
+++ src/cmps/board cmps/BoardSort/SortRow.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { List, ListItem, MenuTitle } from "@vibe/core";
+import { Dropdown, List, ListItem, MenuTitle } from "@vibe/core";
 
 const SortRow = () => {
 
@@ -13,9 +13,40 @@ const SortRow = () => {
     // );
 
     return (
-        <section className="filter-column">
-            <List>
-            </List>
+        <section className="sort-row">
+            <Dropdown
+                className="sort-list"
+                options={[
+                    {
+                        label: 'Option 1',
+                        value: 1
+                    },
+                    {
+                        label: 'Option 2',
+                        value: 2
+                    },
+                    {
+                        label: 'Option 3',
+                        value: 3
+                    }
+                ]}
+                placeholder="Placeholder text here"
+            />
+            <Dropdown
+                clearable={false}
+                defaultOptions={ }
+                className="ascending-decsending"
+                options={[
+                    {
+                        label: 'Ascending',
+                        value: 1,
+                    },
+                    {
+                        label: 'Decsending',
+                        value: -1,
+                    }
+                ]}
+            />
         </section>
     );
 };
