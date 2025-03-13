f296c50660ebd2f1b25ba072c36c4394bf83907b
Afik Yefet <afik.yefet@gmail.com>
2025-03-13 13:47:52 +0200

    merge finished

--- src/cmps/board cmps/BoardHeaderContextualActions.jsx
+++ src/cmps/board cmps/BoardHeaderContextualActions.jsx
@@ -1,5 +1,8 @@
 import { Button, Flex, Icon, MenuButton, MenuItem, SplitButton, SplitButtonMenu } from "@vibe/core"
 import { Filter, Group, Person, Search, Show, Sort } from "@vibe/icons"
+import { BoardPerson } from "./BoardPerson/BoardPerson"
+import { BoardFilter } from "./BoardFilter/BoardFilter"
+import { BoardSort } from "./BoardSort/BoardSort"
 
 const BoardHeaderContextualActions = ({ onAddGroup, onAddTask }) => {
     return <Flex className="action" gap="small" align="center">
