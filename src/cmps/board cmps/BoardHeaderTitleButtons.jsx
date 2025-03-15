b0aba5ab94facbc3f52a346431834e79b77b6830
= <dorcohen236@gmail.com>
2025-03-15 16:17:48 +0200

    general css changes for layout

--- src/cmps/board cmps/BoardHeaderTitleButtons.jsx
+++ src/cmps/board cmps/BoardHeaderTitleButtons.jsx
@@ -1,15 +1,88 @@
-import { Button, Flex, Heading, IconButton, MenuButton } from "@vibe/core";
-import { Notifications } from "@vibe/icons";
+import { Button, Flex, Heading, IconButton, MenuButton, Dialog, Icon, Tooltip, DialogContentContainer, Text, Link } from "@vibe/core";
+import { Notifications, DropdownChevronDown, DropdownChevronUp } from "@vibe/icons";
 import { useState } from "react";
 import { useSelector } from "react-redux";
+import { boardService } from "../../services/board/board.service.local";
+import {
+    Modal,
+    ModalHeader,
+    ModalContent,
+    ModalMedia,
+    ModalFooter,
+    ModalFooterWizard,
+    ModalBasicLayout,
+    ModalSideBySideLayout,
+    ModalMediaLayout
+  } from "@vibe/core/next";
 
 const BoardHeaderTitleButtons = ({ boardName }) => {
+    const [isDialogOpen, setDialogOpen] = useState(false);
     const board = useSelector(storeState => storeState.boardModule.board)
     const [numberOfMembers, setNumberOfMembers] = useState(0);
 
+    
+    const handleCloseDialog = () => {
+        setDialogOpen(false);
+    };
+
+    const handleOpenDialog = (event) => {
+        event.stopPropagation();
+        setDialogOpen((prev) => !prev);
+    };
+
+    const currentDate = new Date().toLocaleString('en-GB', { day: '2-digit', month: '2-digit', year: 'numeric', hour: '2-digit', minute: '2-digit', hour12: false }).replace(',', '');
+    console.log(board)
+
 
     return <>
-        <Heading type="h2" className="title">{board.name}</Heading>
+        <Dialog
+            modifiers={[{ name: "preventOverflow", options: { mainAxis: false } }]}
+            open={isDialogOpen}
+            showTrigger={isDialogOpen ? [] : null}
+            onClickOutside={isDialogOpen && handleCloseDialog}
+            position="bottom"
+            zIndex={1010}
+            content={
+                <DialogContentContainer size="large">
+                    <div className="headerDialog">
+                        <Heading type="h4" className="title">
+                            {board.name}
+                        </Heading>
+                        <Text>
+                            A clone of monday.com
+                        </Text>
+                        <hr/>
+                        <div class="headerBoardInfo">
+                            <Heading type="h4" weight="light" className="title">
+                                Board info
+                            </Heading>
+                            <div className="infos">
+                                <div className="info">
+                                    <div className="infoType">
+                                        <Text>
+                                            Created at
+                                        </Text>
+                                    </div>
+                                    <div className="infoValue">
+                                        <Text>
+                                            {currentDate}
+                                        </Text>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </DialogContentContainer>
+            }
+        >
+            <div className={`space-y-4 headerTitle ${isDialogOpen ? "active-header" : ""}`} style={{display: "flex"}}>
+                <Heading type="h2" className="title" onClick={(event) => handleOpenDialog(event)}>
+                    {board.name}
+                </Heading>
+
+                <Icon icon={isDialogOpen ? DropdownChevronUp : DropdownChevronDown} ariaLabel={board.name} iconSize="32" />
+            </div>
+        </Dialog>
 
         <Flex className="actions" gap="small" align="center" justify="end" style={{ height: '36px' }}>
             <IconButton icon={Notifications} ariaLabel="Notifications" badge="1" />
--- src/cmps/board cmps/dynamicCmps/Member.jsx
+++ src/cmps/board cmps/dynamicCmps/Member.jsx
@@ -162,7 +162,7 @@ export function Member({ info, allMembers, onTaskUpdate }) {
                     title="Add Members"
                     width="large"
                     position="top"
-                    zIndex="99"
+                    zIndex="999"
                     className="dialogMembers"
                 >
                     <DialogContentContainer
@@ -320,7 +320,7 @@ export function Member({ info, allMembers, onTaskUpdate }) {
                         />
                     )}
                 <AvatarGroup
-                    size="medium"
+                    size="small"
                     max={avatarMax}
                     counterProps={{
                         ariaLabelItemsName: "teams",
